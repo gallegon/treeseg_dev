7f7867f8aa1dd9b74575964a2183c752c2e3a70e
MooseOnTheRocks <sam.foltz@hotmail.com>
2022-08-06 02:11:52 -0700

    Added pip install pdal for python bindings

--- setupenv.cmd
+++ setupenv.cmd
@@ -2,4 +2,6 @@ set ENV_NAME=treeseg_dev
 
 call conda create -n %ENV_NAME%
 
-call conda env update -n %ENV_NAME% -f environment.yaml
+call conda env update -n %ENV_NAME% -f environment.
+
+pip install pdal
