2911abc92dd2ba90b0c6a905fe7b42bc7ed24641
MooseOnTheRocks <sam.foltz@hotmail.com>
2022-08-28 00:28:16 -0700

    Fixed setupenv.cmd

--- scripts/setupenv.cmd
+++ scripts/setupenv.cmd
@@ -1,5 +1,11 @@
+@echo off
+
 set ENV_NAME=treeseg_dev
 
-call conda create -n %ENV_NAME%
+call conda create -y -n %ENV_NAME%
 
-call conda env update -n %ENV_NAME% -f environment.yaml
+call conda env update -n %ENV_NAME% -f ..\environment.yaml
+
+call conda activate %ENV_NAME%
+
+call python -m pip install pdal
