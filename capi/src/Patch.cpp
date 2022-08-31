b2b1ba67f9259eb75c2377293ebd31d69eb02dd3
gallegon <gallegon@oregonstate.edu>
2022-08-31 12:31:15 -0700

    new helper function get_valid_neighbors() computes if any 4 way neighbors in grid are valid

--- capi/src/Patch.cpp
+++ capi/src/Patch.cpp
@@ -71,12 +71,9 @@ void Patch::add_hierarchy(int hierarchy_id, std::set<std::pair<int, int> >& conn
 
 void Patch::adjust_hierarchy(int hierarchy_id, Centroid hac)
 {
-    //std::cout << "pX: " << this->centroid.first << " pY: ";
-    //std::cout << this->centroid.second << std::endl;
-    //std::cout << "hacX: " << hac.first << " hacY: " << hac.second << std::endl;
 
     double distance = get_distance(this->centroid, hac);
-    //std::cout << "distance: " << distance << std::endl;
+
     // TODO: think about float comparison more
     if (distance < this->closest_hierarchy_dist) {
         this->closest_hierarchy_id = hierarchy_id;
@@ -114,12 +111,13 @@ void Patch::operator=(const Patch& patch) {
     this->sum_y = patch.sum_y;
     this->cell_count = patch.cell_count;
 }
-void addDirectedNeighbor(std::vector<int>& neighbors,  int neighbor_i, int neighbor_j, int n,int current_id,
+void addDirectedNeighbor(std::vector<int>& neighbors, Cell neighbor, int current_id,
                         int current_level, Grid<int>& labels,
                         Grid<int>& levels) {
     // get the feature id and level of the neighboring cell
-    // int neighbor_id = Get2D(labels, neighbor_i, neighbor_j);
-    // int neighbor_level = Get2D(levels, neighbor_i, neighbor_j);
+    int neighbor_i = neighbor.first;
+    int neighbor_j = neighbor.second;
+
     int neighbor_id = labels.get(neighbor_i, neighbor_j);
     int neighbor_level = levels.get(neighbor_i, neighbor_j);
 
@@ -141,73 +139,58 @@ void addDirectedNeighbor(std::vector<int>& neighbors,  int neighbor_i, int neigh
 #define RIGHT 1
 #define LEFT -1
 
+std::vector<Cell> get_valid_neighbors(int i, int j, int* dimensions) {
+    std::vector<Cell> valid_neighbors;
+
+    int m = dimensions[0];
+    int n = dimensions[1];
+
+
+    if ((i - 1) >= 0)
+        valid_neighbors.push_back(std::make_pair(i + TOP, j));
+
+    if ((i + 1) < m)
+        valid_neighbors.push_back(std::make_pair(i + BOTTOM, j));
+
+    if ((j - 1) >= 0)
+        valid_neighbors.push_back(std::make_pair(i, j + LEFT));
+
+    if ((j + 1) < n)
+        valid_neighbors.push_back(std::make_pair(i, j + RIGHT));
+
+    return valid_neighbors;
+
+}
+
 std::vector<int> get_neighbors(int i, int j, int* dimensions, Grid<int>& labels, Grid<int>& levels) {
     // m is the i size of the array, n is the j size
     int m = dimensions[0];
     int n = dimensions[1];
-    int neighbor_id;
-    int neighbor_level;
+
     int neighbor_i, neighbor_j;
+    
     // This is for the valid neighbors of a given cell.  Valid neighbors are
     // calculated below
     std::vector<int> neighbors;
 
-    // TODO: find out a way to not have to cast from system int to npy_intp
-    // int current_id = Get2D(labels, i, j);
-    // int current_level = Get2D(levels, i, j);
+    std::vector<Cell> neighboring_cells;
+
+    
     int current_id = labels.get(i, j);
     int current_level = levels.get(i, j);
 
-    // Old way using non-NumPy arrays
-    //int current_id = labels[IDX(i, j)];
-    //int current_level = levels[IDX(i, j)];
+    ///*
+    neighboring_cells = get_valid_neighbors(i, j, dimensions);
 
-    // get top neighbor
-    if ((i - 1) >= 0) {
-        neighbor_i = i + TOP;
-        neighbor_j = j;
-
-        addDirectedNeighbor(neighbors, neighbor_i, neighbor_j, n, current_id, current_level, labels, levels);
-    }
-    // get bottom neighboring cell
-    if ((i + 1) < m) {
-        neighbor_i = i + BOTTOM;
-        neighbor_j = j;
-
-        addDirectedNeighbor(neighbors, neighbor_i, neighbor_j, n, current_id, current_level, labels, levels);
-
-    }
-    // get left neighbor
-    if ((j - 1) >= 0) {
-        neighbor_i = i;
-        neighbor_j = j + LEFT;
-
-        addDirectedNeighbor(neighbors, neighbor_i, neighbor_j, n, current_id, current_level, labels, levels);
-    }
-
-    // get right neighbor
-    if ((j + 1) < n) {
-        neighbor_i = i;
-        neighbor_j = j + RIGHT;
-
-        addDirectedNeighbor(neighbors, neighbor_i, neighbor_j, n, current_id, current_level, labels, levels);
+    for (auto nc_it = neighboring_cells.begin(); nc_it != neighboring_cells.end(); ++nc_it) {
+        Cell neighbor = *nc_it;
+        addDirectedNeighbor(neighbors, neighbor,current_id, current_level, labels, levels);
     }
 
     return neighbors;
 }
 
 void create_patches(Grid<int>& labels, Grid<int>& levels, struct PdagData& context) {
-    //std::map<int, Patch> patches;
-
-    // Use this dictionary to keep track of patches that have no parent.
-    // These will be the "local maxima" patches
-    //std::map<int, Patch> parentless_patches;
-
-    //c-style array of data
-
-    // int* levelsData = (int*) PyArray_DATA(levels);
-    // int* labelsData = (int*) PyArray_DATA(labels);
-
     // This is an edge list of what will ultimately become the PDAG
     std::set<std::pair<int, int> > connected_patches;
     std::set<std::pair<int, int> >::iterator set_iter;
@@ -226,12 +209,8 @@ void create_patches(Grid<int>& labels, Grid<int>& levels, struct PdagData& conte
     // TODO: implement this 2D loop with NumPy's array iterators.
     for (int i = 0; i < m; ++i) {
         for (int j = 0; j < n; ++j) {
-            // current_feature = Get2D(labels, i, j);
-            // current_level = Get2D(levels, i, j);
             current_feature = labels.get(i, j);
             current_level = levels.get(i, j);
-            // current_feature = (int) *(PyArray_GETPTR2(labels, (npy_intp)i, (npy_intp)j));
-            // current_feature = labelsData[IDX(i, j)];
 
             // Don't compute a patch for a cell with that has id 0
             if (current_feature == 0) {
@@ -256,8 +235,7 @@ void create_patches(Grid<int>& labels, Grid<int>& levels, struct PdagData& conte
 
             // Get the current cell's neighbors, then iterate
             std::vector<int> neighbors = get_neighbors(i, j, dimensions, labels, levels);
-            std::vector<int>::iterator neigh_it;
-            for (neigh_it = neighbors.begin(); neigh_it != neighbors.end(); ++neigh_it) {
+            for (auto neigh_it = neighbors.begin(); neigh_it != neighbors.end(); ++neigh_it) {
                 int neighbor = *neigh_it;
 
 
@@ -265,8 +243,6 @@ void create_patches(Grid<int>& labels, Grid<int>& levels, struct PdagData& conte
                 // neighbor.  If a neighbor in neighbors is negative, then the patch is a child to it's
                 // neighbor
                 if (neighbor < 0) {
-                    //boost::add_edge((neighbor * -1), current_feature, 1, g);
-                    std::cout << "adding edge (" << (neighbor * -1) << ", " << current_feature << ")" << std::endl;
                     connected_patches.insert(std::make_pair((neighbor * -1), current_feature));
                     // Here the current feature is a child patch to some higher level patch.
                     // Therefore this patch cannot be a parentless patch.
@@ -274,8 +250,6 @@ void create_patches(Grid<int>& labels, Grid<int>& levels, struct PdagData& conte
                 }
                 // Here the patch is the parent to some lower patch.
                 else {
-                    std::cout << "adding edge (" << current_feature << ", " << neighbor << ")" << std::endl;
-                    //boost::add_edge(current_feature, neighbor, 1, g);
                     connected_patches.insert(std::make_pair(current_feature, neighbor));
                     context.parentless_patches.erase(neighbor);
                 }
@@ -283,7 +257,7 @@ void create_patches(Grid<int>& labels, Grid<int>& levels, struct PdagData& conte
         }
     }
 
-    DPRINT("Completed edge list");
+    DPRINT("== Completed edge list");
     
     // some abstraction for graph creation
     int parent, child;
@@ -292,7 +266,6 @@ void create_patches(Grid<int>& labels, Grid<int>& levels, struct PdagData& conte
         parent = set_iter->first;
         child = set_iter->second;
 
-        std::cout << "(" << parent << ", " << child << ")" << std::endl;
         if (parent == child) {
             boost::add_edge(parent, child, 0, context.graph);
         }
@@ -302,91 +275,20 @@ void create_patches(Grid<int>& labels, Grid<int>& levels, struct PdagData& conte
         
 
     }
-
-    // Moved to Hierarchy
-    /*
-    context.patches = patches;
-    context.parentless_patches = parentless_patches;
-    context.graph = g;
-    */
-    /*
-    std::cout << "Num of parentless patches = " << context.parentless_patches.size() << std::endl;
-
-
-    typedef boost::graph_traits<DirectedGraph>::vertex_descriptor vertex_descriptor;
-    typedef boost::property_map<DirectedGraph, boost::vertex_index_t>::type IdMap;
-
-    std::vector< int > d(num_vertices(context.graph));
-
-
-    std::vector<vertex_descriptor> pred(boost::num_vertices(context.graph));
-    boost::iterator_property_map<std::vector<vertex_descriptor>::iterator,
-        IdMap,
-        vertex_descriptor,
-        vertex_descriptor&>
-        predmap(pred.begin(), get(boost::vertex_index, context.graph));
-
-    std::vector<int> distvector(num_vertices(context.graph));
-    boost::iterator_property_map<std::vector<int>::iterator,
-        IdMap,
-        int,
-        int&>
-        distmap_vect(distvector.begin(), get(boost::vertex_index, context.graph));
-
-    // Some debug/trace variables.
-    int count = 0;
-    int total_count = parentless_patches.size() - 1;
-    int one_percent_amount = floor(total_count * 0.01);
-    int next_count = one_percent_amount;
-    int total_reachable = 0;
-
-    std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
-    for (it = context.parentless_patches.begin(); it != context.parentless_patches.end(); ++it) {
-
-        int vertex_id = it->first;
-        vertex_descriptor s = vertex(vertex_id, context.graph);
-        dijkstra_shortest_paths(context.graph, s,
-            predecessor_map(predmap)
-            .distance_map(distmap_vect));
-
-        // -- Reachable patches
-        boost::graph_traits<DirectedGraph>::vertex_iterator vi, vend;
-        for (boost::tie(vi, vend) = vertices(context.graph); vi != vend; ++vi) {
-            if (distvector[*vi] != 2147483647) {
-                // std::cout << "Patch[" << *vi << "]-Distance: " << distvector[*vi] << ", ";
-                total_reachable += 1;
-            }
-        }
-
-        // For debugging/tracing purposes; print an update every 1% of parentless patches processed.
-        if (count >= next_count || count == total_count) {
-            int p = ((float) count / total_count) * 100;
-            std::cout << "Patch ID: " << vertex_id << " :: " << (count + 1) << "/" << context.parentless_patches.size() << " = " << p << "%" << std::endl;
-            auto step_time = (std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now() - begin).count()) / 1000000.0;
-            std::cout << "    -- Time since last update (seconds): " << step_time << std::endl;
-            next_count += one_percent_amount;
-            begin = std::chrono::steady_clock::now();
-        }
-        count += 1;
-    }
-
-    std::cout << "Total reachable nodes from parentless patches = " << total_reachable << std::endl;
-    std::cout << "Number of parentless patches = " << context.parentless_patches.size() << std::endl;
-    std::cout << "Average reachable nodes per parentless patch = " << ((float) total_reachable / context.parentless_patches.size()) << std::endl;
-    std::cout << std::endl;
-    */
 }
 
 
 double get_distance(Centroid c1, Centroid c2) {
     double x1, x2, y1, y2;
-    int xs_squared, ys_squared;
+    double xs_squared, ys_squared;
 
     x1 = c1.first;
     x2 = c2.first;
     y1 = c1.second;
     y2 = c2.second;
 
-    // Not true distance, but it's what we need
-    return (sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2)));
+    xs_squared = pow(x1 - x2, 2);
+    ys_squared = pow(y1 - y2, 2);
+
+    return sqrt(xs_squared + ys_squared);
 }
